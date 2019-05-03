38182e31a0789578fc75811a5016c5e2f76008f9
Ben <bbistline02@gmail.com>
2019-05-03 02:22:31 +0100

    Moved stuff.

--- git/pybot/RobotPrac.Command.py
+++
@@ -1,1 +0,0 @@
-from RobotPrac.Func import Name
--- git/pybot/RobotPracFunc.py
+++
@@ -1,6 +0,0 @@
-def Name(name):
-    name1 = name
-    print('hello')
-    message = print('''Hello ''' + name1 + '''! I'm Dova, your assistant!''')  
-    return message
-    
