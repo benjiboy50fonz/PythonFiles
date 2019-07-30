f496989c0c9d9b70561273ff8b4b4afb5efe7748
Ben <bbistline02@gmail.com>
2019-07-30 00:24:39 -0500

    Worked on rook movement.

--- PycharmProjects/PyChess/pieces/rookpiece.py
+++ PycharmProjects/PyChess/pieces/rookpiece.py
@@ -13,3 +13,38 @@ class Rook(Piece):
         self.symbol = chess.Chess.Symbol.rook
 
         self.colorString = chess.Chess.Color.relation[self.faction]
+
+    def move(self, currentX, currentY, moveToX, moveToY):
+        self.getBoard()
+        # Check possible movements based off of current position.
+
+        self.possiblePositions = []
+        self.possibleUp = []
+        self.possibleDown = []
+
+        # NOTE: The following part is different for each individual pieces's move set.
+
+        up = currentX
+        down = 8 - currentX
+
+        for row in range(up):
+            if row == currentX:
+                continue
+                # THIS ACTUALLY SHOULD NOT BE USED. INVESTIGATE.
+                # This is the selected piece; you cannot moveto yourself.
+            elif self.board[row][currentY][1] == chess.Chess.Game.playerColor:
+                self.possibleUp = []
+                # Checks to see if an allied piece
+            else:
+                self.possibleUp.append([row, currentY])
+
+        for row in range(down):
+            if row + currentX == currentX:
+                continue
+            elif self.board[row + currentX][currentY][1] == chess.Chess.Game.playerColor:
+                break
+                # Checks to see of an allied piece is blocking further progress.
+            else:
+                self.possibleUp.append([row + currentX, currentY])
+    def getBoard(self):
+        self.board = chess.Chess.Board.chess
